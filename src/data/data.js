c894a90ba5dbddd88a58eeff9bc84a9ff1fccb62
Nazar <kostyrkon@gmail.com>
2020-04-20 22:07:12 +0100

    Data array of objects that represents temporary API request.

---
+++ src/data/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        color: "yellow",
+        title: "Home todo's",
+        created: "16-04-2020",
+        todoList: [
+            {
+                title: "Do morning exercises",
+                done: false,
+                filtered: true,
+            },
+            {
+                title: "Brush teeth",
+                done: true,
+                filtered: false,
+
+            }
+        ]
+    },
+    {
+        color: "green",
+        title: "Work todo's",
+        created: "15-04-2020",
+        todoList: [
+            {
+                title: "Start working on ToDo app",
+                done: false,
+                filtered: false,
+
+            },
+            {
+                title: "Text Serhii about app concept",
+                done: false,
+                filtered: false,
+
+            }
+        ]
+    },
+    {
+        color: "orange",
+        title: "Shop todo's",
+        created: "12-04-2020",
+        todoList: [
+            {
+                title: "Buy groceries for dinner (pizza ingredients)",
+                done: false,
+                filtered: false,
+            },
+            {
+                title: "Buy candies for kids",
+                done: true,
+                filtered: false,
+
+            }
+        ]
+    }
+];
+
+export default data
