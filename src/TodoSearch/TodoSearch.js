917f316b7e48476cbc0476bf3718e2bb6140a4cb
Nazar <kostyrkon@gmail.com>
2020-04-27 19:05:19 +0100

    TodoSearch styles.css

--- src/TodoSearch/TodoSearch.js
+++ src/TodoSearch/TodoSearch.js
@@ -1,17 +1,16 @@
 import React from "react";
-
+import './styles.css'
 function TodoSearch({onClear,onChange,value}) {
 
     return(
-        <div className='todoListSearchTitle'>
+        <div className='searchTodoListByTitle'>
 
-        <input type='text'
-                name='search'
-                value={value}
-                onChange={(e)=>onChange(e.target.value)}
-        />
-
-        <button type='submit' onClick={onClear}>X</button>
+                <input type='text'
+                       name='search'
+                       value={value}
+                       onChange={(e)=>onChange(e.target.value)}
+                />
+                <button type='submit' onClick={onClear}>X</button>
 
         </div>
     )
