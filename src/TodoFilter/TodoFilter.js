b128234a35e258e43314cdae7a165b71b924ba22
Nazar <kostyrkon@gmail.com>
2020-05-15 10:18:36 +0100

    Show function moved to setFiltered

--- src/TodoFilter/TodoFilter.js
+++ src/TodoFilter/TodoFilter.js
@@ -1,27 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function TodoFilter({show}) {
-    const [isFiltered, setIsFiltered] = useState(false)
+function TodoFilter({ show }) {
+  const [isFiltered, setIsFiltered] = useState(false);
 
-    const setFiltered =(value)=> {
-        setIsFiltered(!value)
-    }
+  const setFiltered = (value) => {
+    setIsFiltered(!value);
+    show(value);
+  };
 
-    return(
-        <div>
-            <input type='checkbox'
-                   value={isFiltered}
-                   onChange={(e) => setFiltered(e.target.value)
-                                        /*show(e.target.value)*/}
+  return (
+    <div>
+      <input
+        type="checkbox"
+        value={isFiltered}
+        onChange={(e) => setFiltered(e.target.value)}
+      />
 
-                   onClick={()=> show(isFiltered)}
-            />
-
-            <label>Show done</label>
-
-
-        </div>
-    )
+      <label>Show done</label>
+    </div>
+  );
 }
 
-export default TodoFilter
+export default TodoFilter;
