af86b872fd48a1014f4b78bccf86c7bd12513b99
Serhii <serhii@a-dam.com>
2020-05-20 13:07:49 +0200

    Revert "Test"
    This reverts commit e7a2ee1ab1352441dedc26fa85c6347337ffec8d.

--- src/TodoGroup/TodoGroup.js
+++ src/TodoGroup/TodoGroup.js
@@ -8,34 +8,34 @@ import AddTodo from "../AddTodo/AddTodo";
 
 function TodoGroup(props) {
    const { todoList, color, created, title, id, handleCheckbox } = props;
-   // const todoItems = todoList.map((item, index) => {
-   //    if (item["filtered"] && item.filtered !== false) {
-   //       return (
-   //          <TodoItem
-   //             key={index}
-   //             title={item.title}
-   //             done={item.done}
-   //             itemId={item.itemId}
-   //             handleTodoCheckbox={(todoId, done) => {
-   //                handleCheckbox(id, todoId, done);
-   //             }}
-   //          />
-   //       );
-   //    }
+   const todoItems = todoList.map((item, index) => {
+      if (item["filtered"] && item.filtered !== false) {
+         return (
+            <TodoItem
+               key={index}
+               title={item.title}
+               done={item.done}
+               itemId={item.itemId}
+               handleTodoCheckbox={(todoId, done) => {
+                  handleCheckbox(id, todoId, done);
+               }}
+            />
+         );
+      }
 
-   // return null;
-   // });
+      return null;
+   });
 
    return (
       <div className="todoGroup">
-         {/*<div className="titleDateContainer" style={{ borderTopColor: color }}>*/}
-         {/*   <label className="groupTitle">{title}</label>*/}
-         {/*   <label className="groupDate">{created}</label>*/}
-         {/*</div>*/}
+         <div className="titleDateContainer" style={{ borderTopColor: color }}>
+            <label className="groupTitle">{title}</label>
+            <label className="groupDate">{created}</label>
+         </div>
 
-         {/*<div className="itemsContainer">{todoItems}</div>*/}
+         <div className="itemsContainer">{todoItems}</div>
 
-         {/*<AddTodo />*/}
+         <AddTodo />
       </div>
    );
 }
