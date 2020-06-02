c3ea5c17888c6755c6717ca20db903ccd02ed64c
Nazar <kostyrkon@gmail.com>
2020-06-02 17:33:34 +0100

    Added filter property to each group in DEFAULT_DATA;

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,46 +1,52 @@
-export const COLOR_LIST = ['yellow', 'red','green','blue','white'];
+export const COLOR_LIST = ["yellow", "red", "green", "blue", "white"];
 
 export const DEFAULT_DATA = [
-    {
-        id: 1,
-        color: "yellow",
-        title: "Home todo's",
-        created: "16-04-2020",
-        todoList:[]
-    },
-    {
-        id: 2,
-        color: "green",
-        title: "Work todo's",
-        created: "15-04-2020",
-        todoList:[]
-    },
-    {
-        id: 3,
-        color: "orange",
-        title: "Shop todo's",
-        created: "12-04-2020",
-        todoList:[]
-    },
-    {
-        id: 4,
-        color: "blue",
-        title: "Work out todo's",
-        created: "16-04-2020",
-        todoList:[]
-    },
-    {
-        id: 5,
-        color: "red",
-        title: "reading",
-        created: "15-04-2020",
-        todoList:[]
-    },
-    {
-        id: 6,
-        color: "black",
-        title: "Cooking",
-        created: "12-04-2020",
-        todoList:[]
-    }
+   {
+      id: 1,
+      color: "yellow",
+      title: "Home todo's",
+      created: "16-04-2020",
+      filter: true,
+      todoList: [],
+   },
+   {
+      id: 2,
+      color: "green",
+      title: "Work todo's",
+      created: "15-04-2020",
+      filter: true,
+      todoList: [],
+   },
+   {
+      id: 3,
+      color: "orange",
+      title: "Shop todo's",
+      created: "12-04-2020",
+      filter: true,
+      todoList: [],
+   },
+   {
+      id: 4,
+      color: "blue",
+      title: "Work out todo's",
+      created: "16-04-2020",
+      filter: true,
+      todoList: [],
+   },
+   {
+      id: 5,
+      color: "red",
+      title: "reading",
+      created: "15-04-2020",
+      filter: true,
+      todoList: [],
+   },
+   {
+      id: 6,
+      color: "black",
+      title: "Cooking",
+      created: "12-04-2020",
+      filter: true,
+      todoList: [],
+   },
 ];
