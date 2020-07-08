9fde61f24407e48bf3a8284507fbb2c7a656650e
Serhii <serhii@a-dam.com>
2020-07-08 22:57:32 +0200

    Comments

--- src/TodoGroup/TodoGroup.js
+++ src/TodoGroup/TodoGroup.js
@@ -35,17 +35,18 @@ function TodoGroup(props) {
          id: id,
          sortByColor: null,
          sortByCreated: null,
-         color: color,
+         color: color, // no need override parameter that have same key name
          title: title,
          created: created,
-         todoList: todoList,
+         todoList: [...todoList, item], //
       };
 
+      // 1. Why you need this check?
       if (group.id === groupId) {
          const { todoList } = group;
          //const updatedGroup = { ...group, todoList: [...todoList, item] };
          todoList.push(item);
-         postTodo(group.id, item);
+         postTodo(group.id, item); // 3. What are you posting here?
       }
    };
 
@@ -68,8 +69,8 @@ TodoGroup.propTypes = {
    created: PropTypes.string,
    title: PropTypes.string,
    id: PropTypes.number,
-   todoItems: PropTypes.array,
-   todoList: PropTypes.array,
+   todoItems: PropTypes.array, // 3. What difference between this
+   todoList: PropTypes.array, // and this?
    nextItemId: PropTypes.number,
    todoCheckBox: PropTypes.func,
    postTodo: PropTypes.func,
@@ -80,7 +81,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-   postTodo: (id, todo) => dispatch(postTodo(id, todo)),
+   postTodo: (id, todo) => dispatch(postTodo(id, todo)), // 4. are you posting todo or todoGroup?
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoGroup);
